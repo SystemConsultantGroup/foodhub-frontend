9e08f3b8c38a65ef80638b5b67a7f1282c94f453
sera2002 <ouioui2@g.skku.edu>
2023-07-25 14:35:07 +0900

    chore: 코드 스타일 변경(컨벤션 참조), 리드미 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,9 @@
 module.exports = {
-  root: true,
   env: {
     browser: true,
-    es5: true,
+    es2021: true,
   },
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "next/core-web-vitals",
-    "prettier",
-  ],
+  extends: ["plugin:react/recommended", "google", "next/core-web-vitals", "prettier"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
@@ -21,17 +15,6 @@ module.exports = {
   plugins: ["react", "@typescript-eslint"],
   rules: {
     "require-jsdoc": "off",
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-empty-interface": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      {
-        vars: "all",
-        varsIgnorePattern: "^_",
-        args: "after-used",
-        argsIgnorePattern: "^_",
-      },
-    ],
-    quotes: ["error", "double"],
+    quotes: ["error", "double"], // 문자열 들을 쌍따옴표로 감싸도록 강제
   },
 };
