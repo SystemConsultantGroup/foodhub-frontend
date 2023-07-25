bc85c53a7143cf02543a2d7287472d4085b3a8c8
jaychang99 <jaychang99@gmail.com>
2023-07-25 21:52:36 +0900

    chore: lint-staged 설정

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,16 @@
-const lintStagedConfig = {
-  "**/*.ts?(x)": () => "tsc-files -p tsconfig.json --noEmit --pretty",
-};
 
-module.exports = lintStagedConfig;
+module.exports = {
+  "*.{js,jsx}": [
+    "eslint",
+  ],
+  "*.{ts,tsx}": [
+    () => "tsc --skipLibCheck --noEmit -p tsconfig.json",
+    "eslint",
+  ],
+}
+
+// const lintStagedConfig = {
+//   "**/*.ts?(x)": () => "tsc-files -p tsconfig.json --noEmit --pretty",
+// };
+
+// module.exports = lintStagedConfig;
