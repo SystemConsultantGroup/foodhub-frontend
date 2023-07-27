e51ec1a958dce0f0f60bf47832ce68b67c29bdf5
jaychang99 <jaychang99@gmail.com>
2023-07-27 22:43:37 +0900

    feat(Emotion): 모든 font Weight 추가

--- src/styles/fontFace.ts
+++ src/styles/fontFace.ts
@@ -6,6 +6,44 @@ export const fontFace = css`
   @font-face {
     font-family: "Pretendard";
     font-style: normal;
+    font-weight: 100;
+
+    src:
+      local("Pretendard Thin"),
+      local("Pretendard-Thin"),
+      url("../fonts/pretredard/Pretendard-Thin.ttf") format("woff2"),
+      url("../fonts/pretendard/Pretendard-Thin.woff") format("woff"),
+      url("../fonts/pretendard/Pretendard-Thin.ttf") format("truetype");
+  }
+
+  @font-face {
+    font-family: "Pretendard";
+    font-style: normal;
+    font-weight: 200;
+
+    src:
+      local("Pretendard ExtraLight"),
+      local("Pretendard-ExtraLight"),
+      url("../fonts/pretredard/Pretendard-ExtraLight.ttf") format("woff2"),
+      url("../fonts/pretendard/Pretendard-ExtraLight.woff") format("woff"),
+      url("../fonts/pretendard/Pretendard-ExtraLight.ttf") format("truetype");
+  }
+
+  @font-face {
+    font-family: "Pretendard";
+    font-style: normal;
+    font-weight: 300;
+    src:
+      local("Pretendard Light"),
+      local("Pretendard-Light"),
+      url("../fonts/pretendard/Pretendard-Light.woff2") format("woff2"),
+      url("../fonts/pretendard/Pretendard-Light.woff") format("woff"),
+      url("../fonts/pretendard/Pretendard-Light.ttf") format("truetype");
+  }
+
+  @font-face {
+    font-family: "Pretendard";
+    font-style: normal;
     font-weight: 400;
     src:
       local("Pretendard Regular"),
@@ -18,6 +56,30 @@ export const fontFace = css`
   @font-face {
     font-family: "Pretendard";
     font-style: normal;
+    font-weight: 500;
+    src:
+      local("Pretendard Medium"),
+      local("Pretendard-Medium"),
+      url("../fonts/pretendard/Pretendard-Medium.woff2") format("woff2"),
+      url("../fonts/pretendard/Pretendard-Medium.woff") format("woff"),
+      url("../fonts/pretendard/Pretendard-Medium.ttf") format("truetype");
+  }
+
+  @font-face {
+    font-family: "Pretendard";
+    font-style: normal;
+    font-weight: 600;
+    src:
+      local("Pretendard SemiBold"),
+      local("Pretendard-SemiBold"),
+      url("../fonts/pretendard/Pretendard-SemiBold.woff2") format("woff2"),
+      url("../fonts/pretendard/Pretendard-SemiBold.woff") format("woff"),
+      url("../fonts/pretendard/Pretendard-SemiBold.ttf") format("truetype");
+  }
+
+  @font-face {
+    font-family: "Pretendard";
+    font-style: normal;
     font-weight: 700;
     src:
       local("Pretendard Bold"),
@@ -25,6 +87,18 @@ export const fontFace = css`
       url("../fonts/pretendard/Pretendard-Bold.woff2") format("woff2"),
       url("../fonts/pretendard/Pretendard-Bold.woff") format("woff"),
       url("../fonts/pretendard/Pretendard-Bold.ttf") format("truetype");
+  }
+
+  @font-face {
+    font-family: "Pretendard";
+    font-style: normal;
+    font-weight: 800;
+    src:
+      local("Pretendard ExtraBold"),
+      local("Pretendard-ExtraBold"),
+      url("../fonts/pretendard/Pretendard-ExtraBold.woff2") format("woff2"),
+      url("../fonts/pretendard/Pretendard-ExtraBold.woff") format("woff"),
+      url("../fonts/pretendard/Pretendard-ExtraBold.ttf") format("truetype");
   }
 
   @font-face {
