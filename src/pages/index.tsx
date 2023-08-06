c39f3f3ce929305a61288082632af784b9db7331
jaychang99 <jaychang99@gmail.com>
2023-08-06 21:32:33 +0900

    feat: 메인 페이지 임시 문구 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>푸드허브</div>
+      <div>
+        <h1>푸드허브</h1>
+        <p>푸드허브 메인 페이지입니다. </p>
+      </div>
     </>
   );
 }
