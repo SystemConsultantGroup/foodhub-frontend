88047e8cb61a3484352cefa5177fc73ccaca2cbb
sera2002 <ouioui2@g.skku.edu>
2023-08-08 15:47:53 +0900

    feat(textInput): TextInput 스타일 입힘

---
+++ src/components/inputs/TextInput/TextInput.tsx
@@ -0,0 +1,174 @@
+import React, { useState, useEffect } from "react";
+import styled from "@emotion/styled";
+import { css, Theme } from "@emotion/react";
+
+interface Props {
+  label?: string;
+  placeholder?: string;
+  conditionList?: string[];
+  conditionChecks?: ((text: string) => boolean)[];
+  multiline?: boolean;
+  maxHeight?: number;
+}
+
+const TextInput: React.FC<Props> = ({
+  label,
+  placeholder = "",
+  conditionList,
+  conditionChecks,
+  multiline = false,
+  maxHeight,
+}) => {
+  const [value, setValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
+  const [isFocused, setIsFocused] = useState(false);
+  const [borderStyle, setBorderStyle] = useState("default");
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+    setIsValid(conditionChecks ? conditionChecks.every((check) => check(value)) : true);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const newValue = event.target.value;
+    setValue(newValue);
+
+    // 입력된 텍스트의 조건 확인을 수행하여 isValid 상태 업데이트
+    if (isFocused) {
+      setIsValid(conditionChecks ? conditionChecks.every((check) => check(newValue)) : true);
+    }
+  };
+
+  useEffect(() => {
+    if (isValid) {
+      isFocused
+        ? setBorderStyle("focused")
+        : value
+        ? setBorderStyle("blur")
+        : setBorderStyle("default");
+    } else {
+      setBorderStyle("invalid");
+    }
+  }, [isValid, isFocused, value]);
+
+  const InputComponent = multiline ? StyledTextarea : StyledInput;
+
+  return (
+    <EmotionWrapper>
+      {label && <StyledLabel>{label}</StyledLabel>}
+      {conditionList && (
+        <ConditionListWrapper>
+          {conditionList.map((condition, index) => (
+            <StyledCondition key={index}>{condition}</StyledCondition>
+          ))}
+        </ConditionListWrapper>
+      )}
+      <InputComponent
+        value={value}
+        placeholder={placeholder}
+        onChange={handleChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        style={{ maxHeight }}
+        isValid={isValid}
+        isFocused={isFocused}
+        borderStyle={borderStyle}
+      />
+    </EmotionWrapper>
+  );
+};
+
+export default TextInput;
+
+const EmotionWrapper = styled.div<Props>`
+  width: 100%;
+  text-align: left;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+const ConditionListWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  gap: 4px;
+`;
+
+const StyledCondition = styled.span`
+  font-size: 12px;
+  font-weight: 300;
+  margin-left: 2px;
+  color: ${({ theme }) => theme.color.gray400};
+`;
+
+const StyledLabel = styled.span`
+  color: ${({ theme }) => theme.color.gray600};
+  font-size: 16px;
+  font-weight: 300;
+  margin-left: 2px;
+`;
+
+const commonStyles = (theme: Theme, isValid: boolean, isFocused: boolean, borderStyle: string) => {
+  return css`
+    width: 100%;
+    padding: 6px;
+    font-size: 14px;
+    border-radius: 6px;
+
+    line-height: 1.5;
+
+    ${getBorderStyles(theme, borderStyle)};
+    color: ${isValid ? theme.color.gray600 : theme.color.danger600};
+    box-shadow: ${!isValid && theme.shadow.default};
+
+    &::placeholder {
+      color: ${theme.color.gray400};
+      font-size: 14px;
+      font-weight: 300;
+    }
+  `;
+};
+
+const StyledInput = styled.input<
+  Props & { isValid: boolean; isFocused: boolean; borderStyle: string }
+>`
+  ${({ theme, isValid, isFocused, borderStyle }) =>
+    commonStyles(theme, isValid, isFocused, borderStyle)};
+`;
+
+const StyledTextarea = styled.textarea<
+  Props & { isValid: boolean; isFocused: boolean; borderStyle: string }
+>`
+  ${({ theme, isValid, isFocused, borderStyle }) =>
+    commonStyles(theme, isValid, isFocused, borderStyle)};
+  resize: none;
+`;
+
+const getBorderStyles = (theme: Theme, variant: string) => {
+  switch (variant) {
+    case "focused":
+      return css`
+        outline: none;
+        border: 1.5px solid ${theme.color.primary500};
+        box-shadow: ${theme.shadow.default};
+      `;
+    case "blur":
+      return css`
+        border: 1px solid ${theme.color.primary700};
+      `;
+    case "invalid":
+      return css`
+        border: 0.5px solid ${theme.color.danger600};
+        box-shadow: ${theme.shadow.default};
+      `;
+    default:
+      return css`
+        border: 0.5px solid ${theme.color.gray500};
+      `;
+  }
+};
