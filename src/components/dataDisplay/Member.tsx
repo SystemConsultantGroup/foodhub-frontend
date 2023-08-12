7dc44ca7483d5037ff03d6c46e3214a7b09fba4c
jaychang99 <jaychang99@gmail.com>
2023-08-12 20:52:09 +0900

    feat: Member 카드 기본 퍼블리싱

---
+++ src/components/dataDisplay/Member.tsx
@@ -0,0 +1,68 @@
+import Image from "next/image";
+import styled from "@emotion/styled";
+import { textEllipsis } from "styles/ellipsis/textEllipsis";
+
+interface Props {
+  imageSrc?: string;
+  memberName: string;
+  memberDescription: string;
+}
+
+const Member = ({ imageSrc, memberName, memberDescription }: Props) => {
+  const src = imageSrc ?? "/images/defaults/default-member-profile-image.jpeg";
+  const alt = `${memberName} 멤버 프로필 사진`;
+
+  return (
+    <EmotionWrapper>
+      <Image className="member-profile-image" src={src} width={40} height={40} alt={alt} />
+      <div className="member-info">
+        <p className="member-name">{memberName}</p>
+        <p className="member-description">{memberDescription}</p>
+      </div>
+    </EmotionWrapper>
+  );
+};
+
+export default Member;
+
+const EmotionWrapper = styled.div`
+  display: flex;
+  align-items: center;
+
+  padding: 14px 20px;
+  /* background-color: ${({ theme }) => theme.color.primary100}; */
+  border: 1px solid ${({ theme }) => theme.color.gray200};
+  border-radius: 6px;
+  transition: background-color 0.3s ease-in-out;
+
+  &:hover {
+    border: 1px solid ${({ theme }) => theme.color.primary500};
+    /* background-color: ${({ theme }) => theme.color.primary500}; */
+  }
+
+  .member-profile-image {
+    border-radius: 50%;
+    border: 1px solid ${({ theme }) => theme.color.gray200};
+    margin-right: 16px;
+  }
+
+  .member-info {
+    flex: 1;
+    overflow: hidden;
+
+    .member-name {
+      font-size: 18px;
+      font-weight: 700;
+      color: ${({ theme }) => theme.color.gray700};
+      margin-bottom: 3px;
+      ${textEllipsis}
+    }
+
+    .member-description {
+      font-size: 12px;
+      font-weight: 400;
+      color: ${({ theme }) => theme.color.gray500};
+      ${textEllipsis}
+    }
+  }
+`;
