d110d069916c6b535290594fb6957a641c854bd8
sera2002 <ouioui2@g.skku.edu>
2023-08-13 19:23:24 +0900

    temp 1

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -73,10 +73,10 @@ export const EmotionWrapper = styled.button<Props>`
   }
 
   display: flex;
-  width: 100%;
+  width: 100%
   height: 100%
-  justify-content: center
-  align-items: center
+  justify-content: center;
+  align-items: center; 
   text-align: center;
   font-size: 14px;
   font-weight: 300;
