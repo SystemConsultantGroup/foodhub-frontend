01e2324e3da69f9b006420f87bd24ab77c3afcce
sera2002 <ouioui2@g.skku.edu>
2023-08-13 20:28:08 +0900

    chore(tag): DeleteIcon 이름 변경

--- src/components/tag/DeleteIcon.tsx
+++ src/components/tag/DeleteIcon.tsx
@@ -5,10 +5,10 @@ interface Props {
   size?: number;
 }
 
-const CheckIcon: FC<Props> = ({ size = 16 }) => {
+const DeleteIcon: FC<Props> = ({ size = 16 }) => {
   return (
     <Image width={size} height={size} alt="삭제 아이콘" src="/images/icons/x-button-filled.svg" />
   );
 };
 
-export default CheckIcon;
+export default DeleteIcon;
