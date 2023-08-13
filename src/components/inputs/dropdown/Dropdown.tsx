0e0311d1ec14796f3fe70c6db4b7ebe7c6e40972
sera2002 <ouioui2@g.skku.edu>
2023-08-14 00:53:58 +0900

    chore(dropdown): 초기세팅

---
+++ src/components/inputs/dropdown/Dropdown.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+
+interface Props {
+  label?: string;
+}
+
+const Dropdown: React.FC<Props> = () => {
+  const [selectedValue, setSelectedValue] = useState("");
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const newValue = event.target.value;
+    setSelectedValue(newValue);
+  };
+
+  return (
+    <EmotionWrapper>
+      <select value={selectedValue} onChange={handleSelectChange}>
+        <option value="option1">Option 1</option>
+        <option value="option2">Option 2</option>
+        <option value="option3">Option 3</option>
+      </select>
+    </EmotionWrapper>
+  );
+};
+
+export default Dropdown;
+
+const EmotionWrapper = styled.div<Props>``;
