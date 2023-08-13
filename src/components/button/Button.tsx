ebcd936fe8ed608271a8e8ce649028b43d9b0e00
sera2002 <ouioui2@g.skku.edu>
2023-08-13 19:18:24 +0900

    husky 작동 확인

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -74,9 +74,9 @@ export const EmotionWrapper = styled.button<Props>`
 
   display: flex;
   width: 100%;
-  height: 100%;
-  justify-content: center;
-  align-items: center; 
+  height: 100%
+  justify-content: center
+  align-items: center
   text-align: center;
   font-size: 14px;
   font-weight: 300;
