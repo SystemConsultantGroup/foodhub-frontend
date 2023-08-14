f8564fe65e20df7502e66088d1b5bdeff60f4f43
sera2002 <ouioui2@g.skku.edu>
2023-08-14 09:58:57 +0900

    chore(dropdown): DropDown, DropUp 아이콘 추가

--- src/components/dropdown/Dropdown.tsx
+++ src/components/dropdown/Dropdown.tsx
@@ -1,13 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, ReactNode } from "react";
 import styled from "@emotion/styled";
+import DropDownIcon from "components/dropdown/items/DropDownIcon";
+import DropUpIcon from "components/dropdown/items/DropUpIcon";
 
 interface Props {
   name: string;
   label?: string;
+  children: ReactNode;
 }
 
-const Dropdown: React.FC<Props> = () => {
+const Dropdown: React.FC<Props> = ({ children, label, name }: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
   const [selectedValue, setSelectedValue] = useState("");
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
 
   const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const newValue = event.target.value;
@@ -15,20 +23,54 @@ const Dropdown: React.FC<Props> = () => {
   };
 
   return (
-    <EmotionWrapper>
-      <select value={selectedValue} onChange={handleSelectChange}>
-        <option value="option1">Option 1</option>
-        <option value="option2">Option 2</option>
-        <option value="option3">Option 3</option>
+    <EmotionWrapper isOpen={isOpen}>
+      {label && <span className="label">{label}</span>}
+      <div onClick={toggleDropdown}></div>
+      <select name={name} value={selectedValue} onChange={handleSelectChange}>
+        {children}
       </select>
+      {isOpen ? (
+        <DropUpIcon style={{ position: "absolute" }} />
+      ) : (
+        <DropDownIcon style={{ position: "absolute" }} />
+      )}
     </EmotionWrapper>
   );
 };
 
 export default Dropdown;
 
-const EmotionWrapper = styled.div`
+const EmotionWrapper = styled.div<{ isOpen: boolean }>`
+  position: relative;
+  display: inline-block;
+
   select {
-    color: red;
+    opacity: 0;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    cursor: pointer;
+  }
+
+  span.label {
+    font-size: 16px;
+    color: ${({ theme }) => theme.color.gray600};
+  }
+
+  option {
+    border-radius: 6px;
+    background-color: ${({ theme }) => theme.color.white};
   }
 `;
+
+/*
+select {
+    border-radius: 6px;
+    border: 0.5px solid ${({ theme }) => theme.color.gray500};
+    line-height: 1.5;
+    color: ${({ theme }) => theme.color.gray400};
+  }
+
+*/
