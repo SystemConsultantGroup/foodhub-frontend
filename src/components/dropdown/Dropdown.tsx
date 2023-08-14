7a8b71cd228f81708dfcd6d8bc7252b16fffedde
sera2002 <ouioui2@g.skku.edu>
2023-08-14 11:54:49 +0900

    feat(dropdown): dropdown 기능 구현

--- src/components/dropdown/Dropdown.tsx
+++ src/components/dropdown/Dropdown.tsx
@@ -1,6 +1,6 @@
 import React, { HTMLAttributes, useState } from "react";
 import styled from "@emotion/styled";
-import { Props as OptionProps } from "components/dropDown/items/DropDownOption";
+import DropDownOption, { Props as OptionProps } from "components/dropDown/items/DropDownOption";
 import DropDownIcon from "components/dropDown/items/DropDownIcon";
 import DropUpIcon from "components/dropDown/items/DropUpIcon";
 
@@ -8,39 +8,40 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
   name: string;
   label?: string;
   children: React.ReactNode;
+  onSelectValueChange?: (value: string) => void;
 }
 
-const DropDown = ({ children, label, name }: Props) => {
+const DropDown = ({ children, label, name, onSelectValueChange }: Props) => {
   const [isOpen, setIsOpen] = useState(false);
-  const [selectedValue, setSelectedValue] = useState("");
+  const [selectedText, setSelectedText] = useState<React.ReactNode>();
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
 
-  const handleOptionClick = (value: string) => {
-    setSelectedValue(value);
+  const handleOptionClick = (value: string, text: React.ReactNode, index: number) => {
+    setSelectedText(text);
     setIsOpen(false);
+
+    if (onSelectValueChange) {
+      onSelectValueChange(value);
+    }
   };
 
   return (
     <EmotionWrapper isOpen={isOpen}>
       {label && <span className="label">{label}</span>}
       <div className="selected-option" onClick={toggleDropdown}>
-        {selectedValue || "Select an option"}
-        {isOpen ? (
-          <DropUpIcon style={{ position: "absolute" }} />
-        ) : (
-          <DropDownIcon style={{ position: "absolute" }} />
-        )}
+        <span>{selectedText || "Select an option"}</span>
+        {isOpen ? <DropUpIcon /> : <DropDownIcon />}
       </div>
       <div className="options">
         {React.Children.map(
           children,
-          (child) =>
+          (child, index) =>
             React.isValidElement<OptionProps>(child) &&
             React.cloneElement(child, {
-              onClick: () => handleOptionClick(child.props.value),
+              onClick: () => handleOptionClick(child.props.value, child.props.children, index),
             })
         )}
       </div>
@@ -48,20 +49,44 @@ const DropDown = ({ children, label, name }: Props) => {
   );
 };
 
+DropDown.Option = DropDownOption;
 export default DropDown;
 
 const EmotionWrapper = styled.div<{ isOpen: boolean }>`
   position: relative;
-  display: inline-block;
+  font-size: 14px;
+  width: 100%;
 
-  select {
-    opacity: 0;
-    position: absolute;
-    top: 0;
-    left: 0;
+  div.selected-option {
+    display: flex;
+    gap: 8px;
     width: 100%;
     height: 100%;
+    align-items: center;
+    font-weight: 300;
+
+    padding: 8px;
+    border-radius: 6px;
+    border: 0.5px solid ${({ theme }) => theme.color.gray500};
+    color: ${({ theme }) => theme.color.gray400};
     cursor: pointer;
+
+    span {
+      width: 100%;
+    }
+  }
+
+  div.options {
+    position: absolute;
+    top: 120%;
+    left: 0;
+    width: 100%;
+    overflow-y: auto;
+    border: 0.5px solid ${({ theme }) => theme.color.gray500};
+    border-radius: 6px;
+    background-color: white;
+    box-shadow: ${({ theme }) => theme.shadow.default};
+    display: ${(props) => (props.isOpen ? "block" : "none")};
   }
 
   span.label {
@@ -74,13 +99,3 @@ const EmotionWrapper = styled.div<{ isOpen: boolean }>`
     background-color: ${({ theme }) => theme.color.white};
   }
 `;
-
-/*
-select {
-    border-radius: 6px;
-    border: 0.5px solid ${({ theme }) => theme.color.gray500};
-    line-height: 1.5;
-    color: ${({ theme }) => theme.color.gray400};
-  }
-
-*/
--- src/components/dropdown/items/DropDownOption.tsx
+++ src/components/dropdown/items/DropDownOption.tsx
@@ -1,15 +1,22 @@
-import React, { HTMLAttributes } from "react";
+import React from "react";
 import styled from "@emotion/styled";
 
-export interface Props extends HTMLAttributes<HTMLDivElement> {
+export interface Props {
   value: string;
   disabled?: boolean;
   children: React.ReactNode;
+  onClick?: (value: string) => void;
 }
 
-const DropDownOption: React.FC<Props> = ({ children, value, disabled = false }) => {
+const DropDownOption: React.FC<Props> = ({ children, value, disabled = false, onClick }) => {
+  const handleClick = () => {
+    if (!disabled && onClick) {
+      onClick(value);
+    }
+  };
+
   return (
-    <EmotionWrapper value={value} disabled={disabled}>
+    <EmotionWrapper value={value} disabled={disabled} onClick={handleClick}>
       {children}
     </EmotionWrapper>
   );
@@ -18,16 +25,16 @@ const DropDownOption: React.FC<Props> = ({ children, value, disabled = false }) 
 const EmotionWrapper = styled.div<Props>`
   width: 100%;
   color: ${({ theme }) => theme.color.gray500};
-  background-color: ${({ theme }) => theme.color.white};
+  background-color: ${({ theme, disabled }) =>
+    disabled ? theme.color.gray100 : theme.color.white};
   border: 0.5px solid ${({ theme }) => theme.color.gray300};
   line-height: 2;
   padding-left: 8px;
   font-size: 14px;
   font-weight: 300;
+  cursor: pointer;
 
   &.selected {
-  }
-  &:disabled {
   }
 `;
 
