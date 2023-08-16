d3bfebeed8f5d4d20ce8ed9fc9a198ec7c865471
jaychang99 <jaychang99@gmail.com>
2023-08-16 10:46:18 +0900

    feat: RadioGroup 에 label 추가

--- src/components/radio/items/RadioGroup.tsx
+++ src/components/radio/items/RadioGroup.tsx
@@ -2,16 +2,35 @@ import { ReactNode } from "react";
 import styled from "@emotion/styled";
 
 export interface Props {
+  label?: string;
   children: ReactNode;
 }
 
-const RadioGroup = ({ children }: Props) => {
-  return <EmotionWrapper>{children}</EmotionWrapper>;
+const RadioGroup = ({ children, label }: Props) => {
+  return (
+    <EmotionWrapper>
+      {label && <label>{label}</label>}
+      <div className="radio-item-container">{children}</div>
+    </EmotionWrapper>
+  );
 };
 
 export default RadioGroup;
 
 const EmotionWrapper = styled.div`
-  display: flex;
   gap: 8px;
+  display: flex;
+  flex-direction: column;
+
+  label {
+    font-size: 16px;
+    font-weight: 300;
+    margin-left: 2px;
+    color: ${({ theme }) => theme.color.gray600};
+  }
+
+  .radio-item-container {
+    display: flex;
+    gap: 8px;
+  }
 `;
