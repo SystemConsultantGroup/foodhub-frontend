992ad02f66d3c18c5759e19e41b87d17a80fd493
jaychang99 <jaychang99@gmail.com>
2023-08-27 22:42:34 +0900

    feat(Navbar): 클릭 시 실제 링크 이동 및 스타일링 분기처리 추가

--- src/components/navbar/components/NavbarMenuItem.tsx
+++ src/components/navbar/components/NavbarMenuItem.tsx
@@ -1,9 +1,14 @@
 import styled from "@emotion/styled";
 import { TNavbarMenu } from "components/navbar/types/TNavbarMenu";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const NavbarMenuItem = ({ path, label, icon }: TNavbarMenu) => {
+  const { pathname } = useRouter();
+  const active = pathname === path;
+
   return (
-    <EmotionWrapper>
+    <EmotionWrapper active={active} href={path}>
       {icon}
       <p className="menu-item-name">{label}</p>
     </EmotionWrapper>
@@ -12,15 +17,30 @@ const NavbarMenuItem = ({ path, label, icon }: TNavbarMenu) => {
 
 export default NavbarMenuItem;
 
-const EmotionWrapper = styled.div`
+const EmotionWrapper = styled(Link)<{ active: boolean }>`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   width: 100px;
+  height: 60px;
   padding: 10px auto;
+  text-decoration: none; // TODO: globalStyles 에서 resetAnchorStyle 머지 후 제거
+  color: ${({ theme, active }) => (active ? theme.color.gray700 : theme.color.gray200)};
+  stroke: ${({ theme, active }) => (active ? theme.color.gray700 : theme.color.gray200)};
 
-  .menu-item-name {
-    color: ${({ theme }) => theme.color.gray200};
+  &:hover {
+    color: ${({ theme }) => theme.color.gray500};
+    stroke: ${({ theme }) => theme.color.gray500};
+  }
+
+  transition:
+    color 0.2s ease-in-out,
+    stroke 0.2s ease-in-out;
+
+  svg {
+    path {
+      stroke: inherit;
+    }
   }
 `;
