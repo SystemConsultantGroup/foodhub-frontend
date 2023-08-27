548d79aaf86e227107dd2145e6b1d7d1f45da575
jaychang99 <jaychang99@gmail.com>
2023-08-27 19:13:25 +0900

    feat(Mypage): 마이페이지 메인에 가입된 단체 없을 때 CTA 영역 추가

--- src/feature/mypage/mypage.main/components/MyPageOrganizationInfo.tsx
+++ src/feature/mypage/mypage.main/components/MyPageOrganizationInfo.tsx
@@ -1,10 +1,15 @@
 import styled from "@emotion/styled";
+import EmptyOrganizationCTA from "feature/mypage/mypage.main/components/empty/EmptyOrganizationCTA";
 import MyPageSectionTitle from "feature/mypage/mypage.main/components/typography/MyPageSectionTitle";
 
 const MyPageOrganizationInfo = () => {
+  const orgList = [];
+  const isEmpty = orgList?.length === 0;
+
   return (
     <EmotionWrapper>
       <MyPageSectionTitle>내가 속한 단체 정보</MyPageSectionTitle>
+      {isEmpty && <EmptyOrganizationCTA />}
     </EmotionWrapper>
   );
 };
