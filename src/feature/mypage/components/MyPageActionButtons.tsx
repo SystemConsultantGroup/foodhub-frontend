cf9fd899757a0cb183d09020527c7bcd218513f8
jaychang99 <jaychang99@gmail.com>
2023-08-27 14:51:55 +0900

    fix(Mypage): 마이페이지 내 정보 수정 버튼 공통 컴포넌트로 대체

--- src/feature/mypage/components/MyPageActionButtons.tsx
+++ src/feature/mypage/components/MyPageActionButtons.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import Button from "components/button/Button";
 import Link from "next/link";
 
 interface Props {}
@@ -8,7 +9,7 @@ const MyPageActionButtons = ({}: Props) => {
     <EmotionWrapper>
       {/* TODO: 공통 컴포넌트 Button 으로 대체 */}
       <Link href="/mypage/edit">
-        <button>내 정보 수정</button>
+        <Button>내 정보 수정</Button>
       </Link>
     </EmotionWrapper>
   );
