9380730e1f3e8962ad66a6ceaebcb4cae2e3121d
jaychang99 <jaychang99@gmail.com>
2023-08-27 16:42:03 +0900

    feat: GlobalStyles 에서 anchor 태그 스타일 초기화

---
+++ src/styles/reset/resetAnchorStyle.ts
@@ -0,0 +1,12 @@
+import { css } from "@emotion/react";
+
+export const resetAnchorStyle = css`
+  a,
+  a:link,
+  a:visited,
+  a:focus,
+  a:hover,
+  a:active {
+    text-decoration: none;
+  }
+`;
