47e7f18e6ac6f087e0cf5394fdaa2bd7b71db7ca
jaychang99 <jaychang99@gmail.com>
2023-08-28 23:39:42 +0900

    feat(Organization): 단체 생성 및 수정 페이지 기본 골격 셋업

---
+++ src/feature/organizations/components/OrganizationForm.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+interface Props {
+  isEditMode?: boolean;
+}
+
+const OrganizationForm = ({ isEditMode = false }: Props) => {
+  return <EmotionWrapper>OrganizationForm</EmotionWrapper>;
+};
+
+export default OrganizationForm;
+
+const EmotionWrapper = styled.div``;
---
+++ src/pages/organizations/[organizationId]/edit.tsx
@@ -0,0 +1,7 @@
+import ViewOrganizationEdit from "feature/organizations/organizations.edit/views/ViewOrganizationEdit";
+
+const PageOrganizationEdit = () => {
+  return <ViewOrganizationEdit />;
+};
+
+export default PageOrganizationEdit;
