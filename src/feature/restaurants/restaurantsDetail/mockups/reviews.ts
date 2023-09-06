1c62acd4fb13873a5896532e7d06607f50708c25
sera2002 <ouioui2@g.skku.edu>
2023-09-06 20:06:48 +0900

    feat(restaurant-detail): 리뷰 파트 추가 (진행중)

---
+++ src/feature/restaurants/restaurantsDetail/mockups/reviews.ts
@@ -0,0 +1,122 @@
+import { TReviewPage } from "feature/restaurants/restaurantsDetail/types/TReviewPage";
+
+export const reviewPage1: TReviewPage = {
+  contents: [
+    {
+      id: 1,
+      userId: 1,
+      score: 5,
+      content:
+        "내가 먹어본 수육 중 인생수육임 ㄹㅇ 꼭 먹으셈 두번 먹으셈. 진짜 너무너무너무 맛있음",
+    },
+    {
+      id: 2,
+      userId: 1,
+      score: 3,
+      content:
+        "내가 먹어본 수육 중 인생수육임 ㄹㅇ 꼭 먹으셈 두번 먹으셈. 진짜 너무너무너무 맛있음",
+    },
+    {
+      id: 3,
+      userId: 1,
+      score: 4,
+      content:
+        "내가 먹어본 수육 중 인생수육임 ㄹㅇ 꼭 먹으셈 두번 먹으셈. 진짜 너무너무너무 맛있음",
+    },
+    {
+      id: 4,
+      userId: 1,
+      score: 2,
+      content:
+        "내가 먹어본 수육 중 인생수육임 ㄹㅇ 꼭 먹으셈 두번 먹으셈. 진짜 너무너무너무 맛있음",
+    },
+    {
+      id: 5,
+      userId: 1,
+      score: 5,
+      content:
+        "내가 먹어본 수육 중 인생수육임 ㄹㅇ 꼭 먹으셈 두번 먹으셈. 진짜 너무너무너무 맛있음",
+    },
+  ],
+  pageNumber: 1,
+  pageSize: 5,
+  totalCount: 13,
+  totalPages: 3,
+  totalScore: 4.5,
+};
+
+export const reviewPage2: TReviewPage = {
+  contents: [
+    {
+      id: 6,
+      userId: 1,
+      score: 5,
+      content:
+        "내가 먹어본 수육 중 인생수육임 ㄹㅇ 꼭 먹으셈 두번 먹으셈. 진짜 너무너무너무 맛있음",
+    },
+    {
+      id: 7,
+      userId: 1,
+      score: 3,
+      content:
+        "내가 먹어본 수육 중 인생수육임 ㄹㅇ 꼭 먹으셈 두번 먹으셈. 진짜 너무너무너무 맛있음",
+    },
+    {
+      id: 8,
+      userId: 1,
+      score: 4,
+      content:
+        "내가 먹어본 수육 중 인생수육임 ㄹㅇ 꼭 먹으셈 두번 먹으셈. 진짜 너무너무너무 맛있음",
+    },
+    {
+      id: 9,
+      userId: 1,
+      score: 2,
+      content:
+        "내가 먹어본 수육 중 인생수육임 ㄹㅇ 꼭 먹으셈 두번 먹으셈. 진짜 너무너무너무 맛있음",
+    },
+    {
+      id: 10,
+      userId: 1,
+      score: 5,
+      content:
+        "내가 먹어본 수육 중 인생수육임 ㄹㅇ 꼭 먹으셈 두번 먹으셈. 진짜 너무너무너무 맛있음",
+    },
+  ],
+  pageNumber: 2,
+  pageSize: 5,
+  totalCount: 13,
+  totalPages: 3,
+  totalScore: 4.5,
+};
+
+export const reviewPage3: TReviewPage = {
+  contents: [
+    {
+      id: 11,
+      userId: 1,
+      score: 5,
+      content:
+        "내가 먹어본 수육 중 인생수육임 ㄹㅇ 꼭 먹으셈 두번 먹으셈. 진짜 너무너무너무 맛있음",
+    },
+    {
+      id: 12,
+      userId: 1,
+      score: 3,
+      content:
+        "내가 먹어본 수육 중 인생수육임 ㄹㅇ 꼭 먹으셈 두번 먹으셈. 진짜 너무너무너무 맛있음",
+    },
+    {
+      id: 13,
+      userId: 1,
+      score: 4,
+      content:
+        "내가 먹어본 수육 중 인생수육임 ㄹㅇ 꼭 먹으셈 두번 먹으셈. 진짜 너무너무너무 맛있음",
+    },
+  ],
+  pageNumber: 3,
+  pageSize: 3,
+  totalCount: 13,
+  totalPages: 3,
+  totalScore: 4.5,
+};
---
+++ src/feature/restaurants/restaurantsDetail/types/TReviewPage.ts
@@ -0,0 +1,10 @@
+import { TReviewContent } from "feature/restaurants/restaurantsDetail/types/TReviewContent";
+
+export interface TReviewPage {
+  contents: TReviewContent[];
+  pageNumber: number;
+  pageSize: number;
+  totalCount: number;
+  totalPages: number;
+  totalScore: number;
+}
