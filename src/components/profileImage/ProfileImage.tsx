8762544aec1ec2fae67d5ea4b60c6bee8dfd1e78
jaychang99 <jaychang99@gmail.com>
2023-09-09 14:34:12 +0900

    feat(Profile): 프로필 이미지 6개 디폴트 이미지 추가 및 테두리 추가

--- src/components/profileImage/ProfileImage.tsx
+++ src/components/profileImage/ProfileImage.tsx
@@ -9,13 +9,13 @@ interface Props {
  * 본인 프로필 이미지만을 표시하는 컴포넌트이므로, 내부적으로 인증 및 유저 정보에 따라 데이터를 가져와 이미지를 표시합니다.
  * @return {JSX.Element} 본인 프로필 이미지
  */
-const ProfileImage: React.FC<Props> = ({ size = 30 }) => {
+const ProfileImage: React.FC<Props> = ({ size = 30 }: Props) => {
   const userImageSrc = null; // 유저가 프로필 세팅했을 시
   const defaultPhotoId = 1; // 유저가 프로필 세팅하지 않았을 시 기본 제공 값 (API 에서 받아올 예정)
-  const imageSrc = userImageSrc ?? `/images/profile-image-default-${defaultPhotoId}.png`;
+  const imageSrc = userImageSrc ?? `/images/defaults/profile/${defaultPhotoId}.svg`;
 
   return (
-    <EmotionWrapper>
+    <EmotionWrapper size={size}>
       <Image src={imageSrc} width={size} height={size} alt="프로필 이미지" />
     </EmotionWrapper>
   );
@@ -23,4 +23,9 @@ const ProfileImage: React.FC<Props> = ({ size = 30 }) => {
 
 export default ProfileImage;
 
-const EmotionWrapper = styled.div``;
+const EmotionWrapper = styled.div<Required<Props>>`
+  img {
+    border-radius: ${({ size }) => `${size / 2}px`};
+    border: 1px solid ${({ theme }) => theme.color.gray200};
+  }
+`;
