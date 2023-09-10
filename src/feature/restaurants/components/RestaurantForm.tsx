b1638809fd0e4306745d3ebc97991abaeea6baf7
jaychang99 <jaychang99@gmail.com>
2023-09-10 19:53:47 +0900

    feat(Organization): 맛집 생성 및 수정 페이지 기본 골격 셋업

---
+++ src/feature/restaurants/components/RestaurantForm.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+interface Props {
+  isEditMode?: boolean;
+}
+
+const RestaurantForm = ({ isEditMode = false }: Props) => {
+  return <EmotionWrapper>RestaurantForm</EmotionWrapper>;
+};
+
+export default RestaurantForm;
+
+const EmotionWrapper = styled.div``;
