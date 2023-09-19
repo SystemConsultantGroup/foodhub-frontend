0c097f71102f8ab33c3002bb59bd7ff2c8b1a1a5
jaychang99 <jaychang99@gmail.com>
2023-09-19 21:33:53 +0900

    fix(Main): 메인페이지에 PageMarker 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import ViewMainPage from "feature/main/views/ViewMainPage";
-import PageMarker from "components/pageMarker/PageMarker";
 
 function Home() {
   return (
@@ -12,10 +11,6 @@ function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <ViewMainPage />
-      <PageMarker
-        title="푸드허브 메인 페이지"
-        description="최근 추가된 맛집, 평점이 높은 맛집, 데일리 맛잘알 BEST 단체"
-      />
     </>
   );
 }
