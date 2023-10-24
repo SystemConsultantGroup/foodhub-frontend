860e9afb1a0c59a3f415e45fd3dbaf8d94248669
sera2002 <ouioui2@g.skku.edu>
2023-10-24 18:57:58 +0900

    feat(restaurant-review): 공개여부 설정에 사용되는 컴포넌트를 radio에서 checkbox로 변경

--- src/feature/restaurants/restaurantsReview/components/form/RestaurantsReviewForm.tsx
+++ src/feature/restaurants/restaurantsReview/components/form/RestaurantsReviewForm.tsx
@@ -3,8 +3,8 @@ import Rating from "components/rating/Rating";
 import RestaurantDetailHeaderSection from "feature/restaurants/restaurantsDetail/components/section/RestaurantDetailHeaderSection";
 import Button from "components/button/Button";
 import TextInput from "components/inputs/TextInput/TextInput";
-import { useState } from "react";
-import Radio from "components/radio/Radio";
+import { useState, useCallback } from "react";
+import Checkbox from "components/checkbox/Checkbox";
 
 interface Props {
   restaurantId: string | number | string[];
@@ -17,6 +17,7 @@ interface Props {
 const RestaurantsReviewForm: React.FC<Props> = ({ restaurantId, score = 0, content = "" }) => {
   const [selectedScroe, setSelectedScore] = useState<number>(score);
   const [enteredContent, setEnteredContent] = useState<string>(content);
+  const [isPublic, setIsPublic] = useState<boolean>(true);
 
   /**
    * TODO: restaurantId로 맛집 상세정보 불러오기
@@ -36,17 +37,23 @@ const RestaurantsReviewForm: React.FC<Props> = ({ restaurantId, score = 0, conte
     messageOnError: "20자 이상 작성해주세요.",
   };
 
-  const handleScoreChange = (value: number) => {
+  const handleScoreChange = useCallback((value: number) => {
     setSelectedScore(value);
-  };
+  }, []);
 
-  const handleContentChagne = (value: string) => {
+  const handleContentChagne = useCallback((value: string) => {
     setEnteredContent(value);
-  };
+  }, []);
+
+  const handleSetIsPublic = useCallback((value: string) => {
+    if (value === "true") setIsPublic(true);
+    else setIsPublic(false);
+  }, []);
 
   const handleOnSubmit = () => {
     console.log("점수 : ", selectedScroe);
     console.log("리뷰 내용 : ", enteredContent);
+    console.log("공개 여부 : ", isPublic);
   };
 
   return (
@@ -68,15 +75,18 @@ const RestaurantsReviewForm: React.FC<Props> = ({ restaurantId, score = 0, conte
         conditionCheckList={[contentMaxLengthCheck, contentMinLengthCheck]}
         height="150px"
       />
-      <div className="radioDiv">
-        <Radio.Group>
-          <Radio name="공개" value="public" initialValue>
-            공개
-          </Radio>
-          <Radio name="비공개" value="private">
-            비공개
-          </Radio>
-        </Radio.Group>
+      <div className="right-alignment">
+        <p className="label">리뷰 공개 여부를 설정해주세요.</p>
+        <Checkbox.Group
+          checkedList={[String(isPublic)]}
+          setCheckedItem={(value) => handleSetIsPublic(value)}
+        >
+          <Checkbox.Item value="true">공개</Checkbox.Item>
+          <Checkbox.Item value="false">비공개</Checkbox.Item>
+        </Checkbox.Group>
+        {!isPublic && (
+          <p className="description">비공개로 설정시, 단체 외부인에게는 리뷰가 보이지 않습니다.</p>
+        )}
       </div>
       <Button onClick={handleOnSubmit} fullWidth>
         작성 완료
@@ -92,19 +102,35 @@ const EmotionWrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 40px;
+  gap: 30px;
 
   textarea {
     font-size: 16px;
   }
+
   button {
     font-size: 16px;
   }
 
-  .radioDiv {
+  .right-alignment {
     margin-top: -15px;
     width: 100%;
-    display: flex;
-    justify-content: right;
+  }
+
+  .label {
+    margin-top: 20px;
+    margin-bottom: 10px;
+    margin-left: 2px;
+    font-weight: 500;
+    font-size: 16px;
+    color: ${({ theme }) => theme.color.gray600};
+  }
+
+  .description {
+    margin-top: 5px;
+    margin-left: 2px;
+    font-size: 14px;
+    font-weight: 200;
+    color: ${({ theme }) => theme.color.gray600};
   }
 `;
